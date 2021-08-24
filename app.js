1a8c18ab9c1693ae121ec6645c92e73ba387a445
hassan <mahadi.eete@gmail.com>
2021-08-25 01:34:23 +0300

    Added passport.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require ("ejs");
 const mongoose = require ("mongoose");
-const md5 = require("md5");
+const session = require ("express-session");
+const passport = require ("passport");
+const passportLocalMongoose = require ("passport-local-mongoose");
 
 
 const app = express ();
@@ -14,15 +16,34 @@ app.use (bodyParser.urlencoded ({
   extended: true
 }));
 
+app.use(session({
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize ());
+app.use(passport.session ());
+
+
 /////////////////////// Database for Users ///////////////////////
 mongoose.connect ("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set ("useCreateIndex", true);
 
  const userSchema = new mongoose.Schema ({
    email: String,
    password: String
  });
 
+userSchema.plugin (passportLocalMongoose);
+
 const User = new mongoose.model ("user", userSchema);
+
+passport.use (User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser (User.serializeUser ());
+passport.deserializeUser (User.deserializeUser ());
 
 
 /////////////////////// Get request routes ///////////////////////
@@ -38,16 +59,27 @@ app.get ("/register", function (req, res) {
   res.render ("register");
 });
 
+app.get ("/secrets", function (req, res) {
+  if (req.isAuthenticated ()) {
+    res.render ("secrets");
+  } else {
+    res.redirect ("/register");
+  }
+});
+
+app.get ("/logout", function (req, res) {
+  res.logout ();
+  res.redirect ("/");
+});
+
 /////////////////////// Post request for registration ///////////////////////
 app.post ("/register", function (req, res) {
-  const newUser = new User ({
-    email: req.body.username,
-    password: md5 (req.body.password)
-  });
 
-  newUser.save (function (err) {
-    if (!err) {
-      // Open success page if user registered
+  User.register ({username:req.body.username}, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.render ("register");
+    } else {
       res.render ("registered");
     }
   });
@@ -55,26 +87,22 @@ app.post ("/register", function (req, res) {
 
 /////////////////////// Post request for login ///////////////////////
 app.post ("/login", function (req, res) {
-  const username = req.body.username;
-  const password = md5 (req.body.password)
 
-// Find the user and match password
-  User.findOne ({email: username}, function (err, foundUser) {
+  const user = new User ({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login (user, function (err) {
     if (err) {
-      console.log (err);
+      console.log(err);
     } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render ("Secrets");
-        }
-      }
+      passport.authenticate ("local") (req, res, function () {
+        res.redirect ("/secrets");
+      });
     }
   });
 });
-
-
-
-
 
 
 /////////////////////// Listening PORT ///////////////////////
